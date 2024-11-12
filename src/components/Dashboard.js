33a79790a59746a48115db61bff2fedab2ffeab4
kim-tae-hyun <xogus_04@naver.com>
2024-11-12 16:29:57 +0900

    241112_feat_Admin_Dashboard_어드민 대쉬보드 추가(css미보완 상태)_김태현

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Dashboard.css';
 
 const Dashboard = () => {
-  return <div>admin 페이지 고정 틀</div>;
+  const location = useLocation();
+
+  return (
+    <div className="Dashcoard_wrapper">
+      <div className="logo">LOGO</div>
+      <nav>
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          홈
+        </Link>
+        <Link to="#" className="disabled">
+          회원 관리
+        </Link>
+        <Link to="#" className="disabled">
+          도서 관리
+        </Link>
+        <Link to="#" className="disabled">
+          콘텐츠 관리
+        </Link>
+        <div className="community-section">
+          <Link
+            to="/community/notice"
+            className={location.pathname.includes('/community') ? 'active' : ''}
+          >
+            커뮤니티 관리
+          </Link>
+          <div className="community-submenu">
+            <Link
+              to="/community/notice"
+              className={
+                location.pathname === '/community/notice' ? 'active' : ''
+              }
+            >
+              공지사항 관리
+            </Link>
+            <Link
+              to="/community/posts"
+              className={
+                location.pathname === '/community/posts' ? 'active' : ''
+              }
+            >
+              게시글 관리
+            </Link>
+            <Link
+              to="/community/comments"
+              className={
+                location.pathname === '/community/comments' ? 'active' : ''
+              }
+            >
+              댓글 관리
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Dashboard;
